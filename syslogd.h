8cd03155b77c98ed0377322f0131878593a14c00
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-08-21 00:54:20 +0200

    Fix previous merge bug.

--- syslogd.h
+++ syslogd.h
@@ -28,7 +28,7 @@ FILE *priv_open_utmp(void);
 FILE *priv_open_config(void);
 void  priv_config_parse_done(void);
 int   priv_config_modified(void);
-int   priv_getaddrinfo(char *, char *, char *, struct sockaddr *, size_t);
+int   priv_getaddrinfo(char *, char *, struct sockaddr *, size_t);
 int   priv_getnameinfo(struct sockaddr *, socklen_t, char *, size_t);
 
 /* Terminal message */
