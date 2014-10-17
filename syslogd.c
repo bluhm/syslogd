47223b71446cd37c1184ea8ec78c3881fa05d5c3
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-10-18 00:44:54 +0200

    Missing new line in debug message.

--- syslogd.c
+++ syslogd.c
@@ -709,7 +709,7 @@ tcp_readcb(struct bufferevent *bufev, void *arg)
 	/*
 	 * Drop data received from the forward log server.
 	 */
-	dprintf("loghost \"%s\" did send %zu bytes back",
+	dprintf("loghost \"%s\" did send %zu bytes back\n",
 	    f->f_un.f_forw.f_loghost,
 	    EVBUFFER_LENGTH(f->f_un.f_forw.f_bufev->input));
 	evbuffer_drain(bufev->input, -1);
