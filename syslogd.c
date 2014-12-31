af9c114b22f4baa8f50305a3e38d95431f0a3694
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-12-31 13:04:53 +0100

    Remove empty lines.

--- syslogd.c
+++ syslogd.c
@@ -1417,7 +1417,6 @@ init(void)
 		}
 		if (f->f_program)
 			free(f->f_program);
-
 		if (f->f_host)
 			free(f->f_host);
  
@@ -1491,8 +1490,7 @@ init(void)
                         host[i] = 0;
                         continue;
                 }
-		
-  
+
 		p = cline + strlen(cline);
 		while (p > cline)
 			if (!isspace((unsigned char)*--p)) {
