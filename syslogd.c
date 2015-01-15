33d33f99f5e62d59839e3ef7af04b7478a052917
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-15 21:38:59 +0100

    Only free the TLS context on SIGHUP if we have one.

--- syslogd.c
+++ syslogd.c
@@ -1470,8 +1470,10 @@ init(void)
 
 		switch (f->f_type) {
 		case F_FORWTLS:
-			tls_close(f->f_un.f_forw.f_ctx);
-			tls_free(f->f_un.f_forw.f_ctx);
+			if (f->f_un.f_forw.f_ctx) {
+				tls_close(f->f_un.f_forw.f_ctx);
+				tls_free(f->f_un.f_forw.f_ctx);
+			}
 			/* FALLTHROUGH */
 		case F_FORWTCP:
 			/* XXX Save messages in output buffer for reconnect. */
