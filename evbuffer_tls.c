e00f592a2bc55a51c929911f162a5f5e43c16832
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-02-12 01:39:38 +0100

    Make sure that tls_write() is called with the same parameters.

--- evbuffer_tls.c
+++ evbuffer_tls.c
@@ -168,6 +168,7 @@ buffertls_writecb(int fd, short event, void *arg)
 			    buffertls_writecb, buftls);
 			goto reschedule;
 		case TLS_WRITE_AGAIN:
+			buftls->bt_flags |= BT_WRITE_AGAIN;
 			event_set(&bufev->ev_write, fd, EV_WRITE,
 			    buffertls_writecb, buftls);
 			goto reschedule;
@@ -186,6 +187,7 @@ buffertls_writecb(int fd, short event, void *arg)
 		if (res <= 0)
 			goto error;
 	}
+	buftls->bt_flags &= ~BT_WRITE_AGAIN;
 
 	event_set(&bufev->ev_write, fd, EV_WRITE, buffertls_writecb, buftls);
 	if (EVBUFFER_LENGTH(bufev->output) != 0)
@@ -276,6 +278,7 @@ buffertls_set(struct buffertls *buftls, struct bufferevent *bufev,
 	event_set(&bufev->ev_write, fd, EV_WRITE, buffertls_writecb, buftls);
 	buftls->bt_bufev = bufev;
 	buftls->bt_ctx = ctx;
+	buftls->bt_flags = 0;
 }
 
 void
