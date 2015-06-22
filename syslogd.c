60d116404ca8239be4365f68608b1df0b1971c44
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-06-23 00:27:37 +0200

    Remove duplicate while getopt line.

--- syslogd.c
+++ syslogd.c
@@ -331,7 +331,6 @@ main(int argc, char *argv[])
 	int		 ch, i;
 	int		 lockpipe[2] = { -1, -1}, pair[2], nullfd, fd;
 
-	while ((ch = getopt(argc, argv, "46C:dhnuf:Fm:p:a:s:V")) != -1)
 	while ((ch = getopt(argc, argv, "46C:dhnuf:Fm:p:a:s:U:V")) != -1)
 		switch (ch) {
 		case '4':		/* disable IPv6 */
