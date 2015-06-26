f08babdcc984a5f8643dce989556224dc6363072
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-06-26 19:21:25 +0200

    Fix whitos.

--- syslogd.c
+++ syslogd.c
@@ -2298,7 +2298,7 @@ loghost_parse(char *str, char **proto, char **host, char **port,
 			*(*facility)++ = '\0';
 	} else if (suffix)
 		return (-1);
-	
+
 	*host = str;
 	if (**host == '[') {
 		(*host)++;
