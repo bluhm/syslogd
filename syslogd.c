6a2a924fe4a42288ecd8336f759ed49970b28afb
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-07-06 11:34:19 +0200

    Count numbers of peers.

--- syslogd.c
+++ syslogd.c
@@ -290,7 +290,7 @@ struct peer {
 	char			*p_peername;
 	char			*p_hostname;
 };
-int tcpnum = 0;
+int peernum = 0;
 char hostname_unknown[] = "???";
 
 void	 klog_readcb(int, short, void *);
@@ -875,7 +875,7 @@ tcp_acceptcb(int fd, short event, void *arg)
 		peername = hostname_unknown;
 	}
 	dprintf("Peer addresss and port %s\n", peername);
-	if (tcpnum >= MAXTCP) {
+	if (peernum >= MAXTCP) {
 		snprintf(ebuf, sizeof(ebuf), "syslogd: tcp logger \"%s\" "
 		    "denied: maximum %d reached", peername, MAXTCP);
 		logmsg(LOG_SYSLOG|LOG_WARNING, ebuf, LocalHostName, ADDDATE);
@@ -905,7 +905,7 @@ tcp_acceptcb(int fd, short event, void *arg)
 	dprintf("Peer hostname %s\n", hostname);
 	p->p_peername = peername;
 	LIST_INSERT_HEAD(&peers, p, p_entry);
-	tcpnum++;
+	peernum++;
 	bufferevent_enable(p->p_bufev, EV_READ);
 
 	snprintf(ebuf, sizeof(ebuf), "syslogd: tcp logger \"%s\" accepted",
@@ -958,6 +958,7 @@ tcp_closecb(struct bufferevent *bufev, short event, void *arg)
 	if (p->p_hostname != hostname_unknown)
 		free(p->p_hostname);
 	LIST_REMOVE(p, p_entry);
+	peernum--;
 	free(p);
 }
 
