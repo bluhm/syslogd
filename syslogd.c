125d14e7ff7df4abdb83248e4cdc6eeda8793dac
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-09-01 16:32:19 +0200

    Add -K to usage.

--- syslogd.c
+++ syslogd.c
@@ -1467,10 +1467,10 @@ usage(void)
 {
 
 	(void)fprintf(stderr,
-	    "usage: syslogd [-46dFhnuV] [-a path] [-C CAfile] [-c cert_file]\n"
-	    "\t[-f config_file] [-k key_file] [-m mark_interval]\n"
-	    "\t[-p log_socket] [-S listen_address] [-s reporting_socket]\n"
-	    "\t[-T listen_address] [-U bind_address]\n");
+	    "usage: syslogd [-46dFhnuV] [-a path] [-C send_CAfile]\n"
+	    "\t[-c cert_file] [-f config_file] [-K recv_CAfile] [-k key_file]\n"
+	    "\t[-m mark_interval] [-p log_socket] [-S listen_address]\n"
+	    "\t[-s reporting_socket] [-T listen_address] [-U bind_address]\n");
 	exit(1);
 }
 
