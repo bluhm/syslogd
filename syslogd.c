38b052d168e03098693902dc028c60060ccf3e43
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-09-05 02:58:22 +0200

    Name -K server_CAfile in usage.

--- syslogd.c
+++ syslogd.c
@@ -1467,8 +1467,8 @@ usage(void)
 {
 
 	(void)fprintf(stderr,
-	    "usage: syslogd [-46dFhnuV] [-a path] [-C send_CAfile]\n"
-	    "\t[-c cert_file] [-f config_file] [-K recv_CAfile] [-k key_file]\n"
+	    "usage: syslogd [-46dFhnuV] [-a path] [-C CAfile] [-c cert_file]\n"
+	    "[-f config_file] [-K server_CAfile] [-k key_file]\n"
 	    "\t[-m mark_interval] [-p log_socket] [-S listen_address]\n"
 	    "\t[-s reporting_socket] [-T listen_address] [-U bind_address]\n");
 	exit(1);
