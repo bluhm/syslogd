9ac06da2f33dae2d0d27d2da2f2dadfe8eafe2ba
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-09-17 23:46:35 +0200

    Allow at most 6 digits in TIME-SECFRAC.

--- syslogd.c
+++ syslogd.c
@@ -1598,7 +1598,9 @@ logmsg(int pri, char *msg, char *from, int flags)
 				/* TIME-SECFRAC */
 				msg++;
 				msglen--;
-				while (msglen >= 1 && isdigit(msg[0])) {
+				for (i = 0; i < 6; i++) {
+					if (msglen < 1 || !isdigit(msg[0]))
+						break;
 					msg++;
 					msglen--; 
 				}
