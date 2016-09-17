c4c547bd544f2a9ec6bb153e4d26cbe267e6e423
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-09-18 01:01:31 +0200

    ISO time must have time zone.

--- syslogd.c
+++ syslogd.c
@@ -1609,7 +1609,7 @@ logmsg(int pri, char *msg, char *from, int flags)
 		} else if (msglen >= 20 && msg[4] == '-' && msg[7] == '-' &&
 		    msg[10] == 'T' && msg[13] == ':' && msg[16] == ':' &&
 		    (msg[19] == '.' || msg[19] == 'Z' || msg[19] == '+' ||
-		    msg[19] == '-' || msg[19] == ' ')) {
+		    msg[19] == '-')) {
 			/* FULL-DATE "T" FULL-TIME, RFC 5424 */
 			strlcpy(timestamp, msg, sizeof(timestamp));
 			msg += 19;
@@ -1637,10 +1637,6 @@ logmsg(int pri, char *msg, char *from, int flags)
 				timestamp[25+i] = '\0';
 				msg += 7;
 				msglen -= 7;
-			} else if (msglen >= 1 && msg[0] == ' ') {
-				timestamp[19+i] = '\0';
-				msg++;
-				msglen--;
 			}
 		} else if (msglen >= 2 && msg[0] == '-' && msg[1] == ' ') {
 			/* NILVALUE, RFC 5424 */
