c1d350d7d85c19b0c7bd2be10fe51148ee8ea0fd
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-09-29 16:06:13 +0200

    Add my copyright.

--- privsep.c
+++ privsep.c
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (c) 2003 Anil Madhavapeddy <anil@recoil.org>
+ * Copyright (c) 2016 Alexander Bluhm <bluhm@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
