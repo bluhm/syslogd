2332a4814e12d275e9f2597f5ad986ff82694feb
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-03-14 03:49:17 +0100

    Fatal terminates with die().

--- log.c
+++ log.c
@@ -193,7 +193,7 @@ fatal(const char *emsg, ...)
 	va_start(ap, emsg);
 	vfatalc(errno, emsg, ap);
 	va_end(ap);
-	exit(1);
+	die(0);
 }
 
 void
@@ -204,5 +204,5 @@ fatalx(const char *emsg, ...)
 	va_start(ap, emsg);
 	vfatalc(0, emsg, ap);
 	va_end(ap);
-	exit(1);
+	die(0);
 }
