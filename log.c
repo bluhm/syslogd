8681d04402cf2d4af30560b6b377467e1f2adbbb
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-03-16 01:46:15 +0100

    Reduce system include files in log.

--- log.c
+++ log.c
@@ -17,12 +17,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
 #include <string.h>
 #include <syslog.h>
-#include <errno.h>
 #include <time.h>
 
 #include "log.h"
